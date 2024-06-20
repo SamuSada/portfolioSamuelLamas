da2b292d43e656d59761982e2adf1133612d09c4
Samuel Lamas <samusada95@gmail.com>
2024-06-20 18:12:08 +0200

    Problemas con el SectionProjects, posible solución en mobile

--- App/src/components/SectionProjects.jsx
+++ App/src/components/SectionProjects.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const SectionProjects = () => {
   return (
-    <div className="p-4 m-10 w-auto">
-      <div className="overflow-x-auto rounded-lg bg-zinc-800 bg-opacity-70 border-2 border-zinc-800">
+    <div className="p-4 m-10 w-full max-w-full overflow-x-auto">
+      <div className="rounded-lg bg-zinc-800 bg-opacity-70 border-2 border-zinc-800">
         <table className="min-w-full divide-y divide-zinc-800">
           {/* head */}
           <thead className="bg-zinc-800 text-zinc-400">
@@ -18,13 +18,13 @@ const SectionProjects = () => {
             <tr>
               <th className="px-4 py-2">1</th>
               <td className="px-4 py-2">Simple Coffee Listing</td>
-              <td className="px-4 py-2"><a href="https://github.com/SamuSada/SimpleCoffeeListing-React" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">https://github.com/SimpleCoffeeListing-React</a></td>
+              <td className="px-4 py-2"><a href="https://github.com/SamuSada/SimpleCoffeeListing-React" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">https://github.com/SamuSada/SimpleCoffeeListing-React</a></td>
             </tr>
             {/* row 2 */}
             <tr>
               <th className="px-4 py-2">2</th>
               <td className="px-4 py-2">Timer By Samuel Lamas</td>
-              <td className="px-4 py-2"><a href="https://github.com/SamuSada/timer-bySamuelLamas" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">https://github.com/timer-bySamuelLamas</a></td>
+              <td className="px-4 py-2"><a href="https://github.com/SamuSada/timer-bySamuelLamas" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">https://github.com/SamuSada/timer-bySamuelLamas</a></td>
             </tr>
           </tbody>
         </table>
